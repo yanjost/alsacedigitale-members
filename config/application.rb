4d25f783d35911192ecad1b29b16e0a22d048cf5
Soulou <leo@unbekandt.eu>
2013-07-07 12:08:21 +0200

    configure mongoid

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,11 @@
 require File.expand_path('../boot', __FILE__)
 
-require 'rails/all'
+# Comment for mongoid
+# require 'rails/active_record/railtie'
+require 'action_controller/railtie'
+require 'action_mailer/railtie'
+require 'rails/test_unit/railtie'
+require 'sprockets/railtie'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
