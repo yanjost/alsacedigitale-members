8e7e957b4bc93db7017ee4f6e654de4367238027
MaximeHeckel <heckelmaxime@gmail.com>
2013-07-07 15:02:58 +0200

    Added Payement model

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -7,7 +7,7 @@ class Payment
   field :check_reference, type: String
   field :transfer_reference, type: String
 
-  validates :mode, inclusion: { in: ["Bank Check", "Cash", "Transfer"] } 
+  validates :mode, inclusion: { in: ["Bank Check", "Cash", "Transfer", "Paypal"] } 
   validates_with CheckPaymentValidator
   validates_with TransferPaymentValidator
 
@@ -27,3 +27,48 @@ class Payment
   end
 
 end
+
+##Paypal
+
+def self.conf
+  @@gateway_conf ||= YAML.load_file(Rails.root.join('config/gateway.yml').to_s)[Rails.env]
+end
+
+def setup_payement(options)
+  gateway.setup_payement(amount * 100, options)
+end
+
+def redirect_url(token)
+  gateway.redirect_url(token)
+end
+
+def purchase(options={}) 
+    self.status = PROCESSING  
+    #:ip       => request.remote_ip,
+    #:payer_id => params[:payer_id],
+    #:token    => params[:token]
+    response = gateway.purchase(amt, options)      
+    if response.success?       
+      self.transaction_num = response.params['transaction_id']       
+      self.status = SUCCESS     
+    else       
+      self.status = FAILED     
+    end     
+    return self   
+  rescue Exception => e     
+    self.status = FAILED     
+    return self   
+  end
+
+private   
+def gateway 
+  ActiveMerchant::Billing::Base.mode = auth['mode'].to_sym 
+  ActiveMerchant::Billing::PaypalExpressGateway.new(
+    :login => auth['login'], :password => auth['password'],
+    :signature => auth['signature']) 
+end
+
+  def auth 
+    self.class.conf 
+  end
+end   
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ MembersAlsacedigitale::Application.routes.draw do
   devise_for :users
   root to: "application#index"
 
+  match '/payement'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
